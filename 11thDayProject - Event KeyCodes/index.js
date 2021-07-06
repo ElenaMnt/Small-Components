b185aea80b66e9e19f67897db8c26ec264f8d685
Lorena Munteanu <lorena.munteanu94@gmail.com>
2021-07-06 08:17:59 +0300

    Mini Project -  Event Key Codes

--- 11thDayProject - Event KeyCodes/index.js
+++ 11thDayProject - Event KeyCodes/index.js
@@ -1,51 +1,18 @@
-const progress = document.getElementById('progress');
-const prev = document.getElementById('prev');
-const next = document.getElementById('next');
-const circles = document.querySelectorAll('.circle');
+const insert = document.getElementById('insert');
 
-let currentActive = 1
-
-next.addEventListener('click', () => {
-    currentActive++
-    
-    if(currentActive > circles.length){
-        currentActive = circles.length;
-    }
-    update()
+window.addEventListener('keydown', (event) => {
+    insert.innerHTML=`
+    <div class="key">
+        ${event.key === ' '? 'Space': event.key}
+            <small>event.key</small>
+        </div>
+        <div class="key">
+        ${event.keyCode}    
+            <small>event.keyCode</small>
+        </div>
+        <div class="key">
+        ${event.code}   
+            <small>event.code</small>
+        </div>`
 })
 
-prev.addEventListener('click', () => {
-    currentActive--
-    
-    if(currentActive < 1){
-        currentActive = 1;
-    }    
-    update()
-})
-
-function update(){
-    circles.forEach((circle, idx)=>{
-        if(idx < currentActive){
-            circle.classList.add('active')
-        }
-        else{
-            circle.classList.remove('active');
-        }
-    })
-
-    const actives = document.querySelectorAll('.active')
-
-    progress.style.width = ((actives.length-1) / (circles.length-1)) * 100 + '%'
-
-    if(currentActive === 1){
-        prev.disabled = true;
-    }
-    else if (currentActive === circles.length){
-        next.disabled = true;
-    }
-    else{
-        prev.disabled = false;
-        next.disabled = false;
-    }
-
-}
