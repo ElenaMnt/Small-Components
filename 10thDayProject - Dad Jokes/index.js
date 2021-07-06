c1fd5e47e803fde105297f95d49980adbc32413b
Lorena Munteanu <lorena.munteanu94@gmail.com>
2021-07-06 08:18:53 +0300

    Mini Project -  Dad Jokes (Api)

--- 10thDayProject - Dad Jokes/index.js
+++ 10thDayProject - Dad Jokes/index.js
@@ -1,51 +1,23 @@
-const progress = document.getElementById('progress');
-const prev = document.getElementById('prev');
-const next = document.getElementById('next');
-const circles = document.querySelectorAll('.circle');
+const jokeEl = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
 
-let currentActive = 1
 
-next.addEventListener('click', () => {
-    currentActive++
-    
-    if(currentActive > circles.length){
-        currentActive = circles.length;
+jokeBtn.addEventListener('click', generateJoke)
+
+generateJoke();
+
+function generateJoke(){
+
+    const config = {
+        headers: {
+            'Accept' : 'application/json'
+        }
     }
-    update()
-})
-
-prev.addEventListener('click', () => {
-    currentActive--
-    
-    if(currentActive < 1){
-        currentActive = 1;
-    }    
-    update()
-})
-
-function update(){
-    circles.forEach((circle, idx)=>{
-        if(idx < currentActive){
-            circle.classList.add('active')
-        }
-        else{
-            circle.classList.remove('active');
-        }
+    fetch('https://icanhazdadjoke.com/', config)
+    .then(res => res.json())
+    .then((data) => {
+        jokeEl.innerHTML = data.joke;
     })
-
-    const actives = document.querySelectorAll('.active')
-
-    progress.style.width = ((actives.length-1) / (circles.length-1)) * 100 + '%'
-
-    if(currentActive === 1){
-        prev.disabled = true;
-    }
-    else if (currentActive === circles.length){
-        next.disabled = true;
-    }
-    else{
-        prev.disabled = false;
-        next.disabled = false;
-    }
-
 }
+
+ 
